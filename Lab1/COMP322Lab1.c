54dc6e87586a6b4ba9d1902fa568607001c36913
Andrew Isaac <andrew.isaac.37@my.csun.edu>
2014-09-11 14:32:21 -0700

    Made separate printPID function

--- Lab1/COMP322Lab1.c
+++ Lab1/COMP322Lab1.c
@@ -1,35 +1,56 @@
+// COMP 322 Lab 1 "Time and Forking Children"
+// Andrew Isaac
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/times.h>
+#include <time.h>
 
-int main() {
-
-   int pid = fork();
-   int status;
-   int cReturnVal;
+void printPIDInfo(int pid, int status) {
+   
+   int cReturnVal, i;
    struct tms buf;
-   
+      
    if (pid == 0) {
-   
-      printf("START: %d\n", time());
-      printf("PID: %d, PPID: %d\n",getpid(),getppid());
-      exit(0);
+      printf("PPID: %d, PID: %d\n",getppid(),getpid());
    }
    
    else {
-   
-      waitpid(pid,&status,0);
-      cReturnVal = WIFEXITED(status);
-      printf("PID: %d, PPID: %d, RETVAL: %d\n",getpid(),getppid(),cReturnVal);
+      cReturnVal = WEXITSTATUS(status);
+      printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d\n",getppid(),getpid(),pid,cReturnVal);
       
-      times(&buf);
+      i = times(&buf);
       
-      printf("USER: %jd, SYS: %jd\n", (int)(buf.tms_utime), (int)(buf.tms_stime));
-      printf("CUSER: %jd, CSYS: %jd\n", (int)(buf.tms_cutime), (int)(buf.tms_cstime));
-      printf("STOP: %d\n", time());
-      
+      // printf("Number of ticks since arbitrary time: %ld\n", i);
+      // printf("Number of ticks per second: %ld\n",sysconf(_SC_CLK_TCK));      
+      printf("USER: %ld SYS: %ld\n", buf.tms_utime, buf.tms_stime);
+      printf("CUSER: %ld CSYS: %ld\n", buf.tms_cutime, buf.tms_cstime);     
    }
+}
+
+int main() {
+  
+   time_t * t = NULL;
+   int status, pid;
+   
+   printf("START: %d\n", time(t)); // seconds since UNIX epoch
+   pid = fork();
+      
+   if (pid == 0) { // child process
+      
+      printPIDInfo(pid, 0);
+      exit(27); // arbitrary return value
+   }
+   
+   else { // parent process
+      
+      waitpid(pid,&status,0);
+      printPIDInfo(pid, status);
+   }
+   
+   printf("STOP: %d\n", time(t));  
+   return 0;
 }
