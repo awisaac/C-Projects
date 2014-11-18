0ac539ce8aed11f254f964306b92915b62f4a5f7
Andrew Isaac <andrew.isaac.37@my.csun.edu>
2014-11-18 14:20:38 -0800

    Changed reader's input to stdin so that matrix_gen size | matrix_add size blocks | reader can be used.

--- Lab7/reader.c
+++ Lab7/reader.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 int main(int argc, char **argv) {
    
    int buffer[1024];
    int i, bytes_read;
    
-   if (argc < 2) {
-      fprintf(stderr,"File path argument required.");
-      exit(-1);   
-   }
-     
-   int fd = open(argv[1], O_RDONLY);
-   
-   bytes_read = read(fd, buffer, sizeof(int) * 1024);
+   bytes_read = read(0, buffer, sizeof(int) * 1024);
       
    while (bytes_read > 0) {
       
@@ -31,6 +21,6 @@ int main(int argc, char **argv) {
          }       
       }
       
-      bytes_read = read(fd, buffer, sizeof(int) * 1024);       
+      bytes_read = read(0, buffer, sizeof(int) * 1024);       
    }
 }
