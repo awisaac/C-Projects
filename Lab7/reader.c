3632a4e78d2859ce13bd73d048725567ccba6d18
Andrew Isaac <andrew.isaac.37@my.csun.edu>
2014-11-18 12:27:06 -0800

    Fixed issues with reader.

--- Lab7/reader.c
+++ Lab7/reader.c
@@ -7,18 +7,30 @@
 int main(int argc, char **argv) {
    
    int buffer[1024];
-   int i;
+   int i, bytes_read;
    
    if (argc < 2) {
-      fprintf(stderr,"File path argument required.");   
+      fprintf(stderr,"File path argument required.");
+      exit(-1);   
    }
      
    int fd = open(argv[1], O_RDONLY);
+   
+   bytes_read = read(fd, buffer, sizeof(int) * 1024);
       
-   while (read(fd, buffer, sizeof(int) * 1024) > 0) {
+   while (bytes_read > 0) {
       
-      for (i = 0; i < 1024; i++) {
-         fprintf(stdout,"%d ",buffer[i]);
-      }   
+      if (bytes_read == sizeof(int) * 1024) {      
+         for (i = 0; i < 1024; i++) {
+            fprintf(stdout,"%d ",buffer[i]);
+         } 
+      }      
+      else {
+         for (i = 0; i < bytes_read / sizeof(int); i++) {
+            fprintf(stdout,"%d ",buffer[i]);
+         }       
+      }
+      
+      bytes_read = read(fd, buffer, sizeof(int) * 1024);       
    }
 }
